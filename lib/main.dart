1a51c60176b43ff73490bac37ac633b58e69fdac
Eslam Alaa <zoro12zx@gmail.com>
2022-11-09 15:18:37 +0200

    Eslam Alaa-Theam and Note view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task2/views/notes_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Note App",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const NotesView(),
+    );
   }
 }
